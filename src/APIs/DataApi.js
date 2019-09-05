258537247a5b939426e4424e078c0eb6f7fa5e35
Cláudio Alex de Oliveira (SEPLAG) <M40490@ca.intra>
2019-09-05 15:16:59 -0300

    Chamado

--- src/APIs/DataApi.js
+++ src/APIs/DataApi.js
@@ -1,6 +1,6 @@
 const api = function (destination, options) {
-  //let url = "http://localhost:5000/" + destination;
-  let url = "/" + destination;
+let url = "http://localhost:5000/" + destination;
+// let url = "/" + destination;
 
   const ops = options || {};
   ops.credentials = "include";
